77793689e23ffdccdf917512a01169dca74aacca
Meirzhan Rymbayev <meirzhan_rumbaev@mail.ru>
2016-02-29 00:33:37 +0600

    Initial commit

---
+++ src/main/java/com/mine/springmvc/dao/EmployeeDao.java
@@ -0,0 +1,15 @@
+package com.mine.springmvc.dao;
+
+import com.mine.springmvc.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeDao {
+
+    Employee findById(int id);
+    void saveEmployee(Employee employee);
+    void deleteEmployeeBySsn(String ssn);
+    List<Employee> findAllEmployees();
+    Employee findEmployeeBySsn(String ssn);
+
+}
